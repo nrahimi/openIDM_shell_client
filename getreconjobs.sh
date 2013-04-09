64f72da68c4f574fe2099098eab7584228bd6a22
smof <simon_moffatt@warpmail.net>
2013-04-09 16:47:23 +0100

    updated reconjobs to use constants for server and port

--- getreconjobs.sh
+++ getreconjobs.sh
@@ -2,4 +2,8 @@
 #OpenIDM Shell Client
 #https://github.com/smof/openIDM_shell_client
 #Gets all completed reconcilation jobs
-./geturl.sh http://localhost:8080/openidm/recon
+
+OPENIDM_SERVER=localhost
+OPENIDM_SERVER_PORT=8080
+
+./geturl.sh http://$OPENIDM_SERVER:$OPENIDM_SERVER_PORT/openidm/recon
