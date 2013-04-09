06e0be8891a0ceaf1fd030449dd7a5efccc3678e
smof <simon_moffatt@warpmail.net>
2013-04-09 16:34:19 +0100

    updated getuser to include arg check

--- getuser.sh
+++ getuser.sh
@@ -2,4 +2,12 @@
 #OpenIDM Shell Client
 #https://github.com/smof/openIDM_shell_client
 #Returns a single user as given via argument id
+
+#check that arg is passed
+if [ "$1" = "" ]; then
+	echo "Argument missing.  Requires argument id"
+	echo "Eg. ./getuser.sh jdoe"
+	exit
+fi
+
 ./geturl.sh http://localhost:8080/openidm/managed/user/$1
