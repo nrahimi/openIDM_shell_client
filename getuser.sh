cd7de93f9b324ffcca64636d0ba361460cfaeec3
smof <simon_moffatt@warpmail.net>
2013-04-09 17:29:09 +0100

    updated to parse server and port from settings.json

--- getuser.sh
+++ getuser.sh
@@ -3,8 +3,15 @@
 #https://github.com/smof/openIDM_shell_client
 #Returns a single user as given via argument id
 
-OPENIDM_SERVER=localhost
-OPENIDM_SERVER_PORT=8080
+#check that jq util is present
+JQ_LOC="$(which jq)"
+if [ "$JQ_LOC" = "" ]; then
+   	echo "JSON parser jq not found.  Download from http://stedolan.github.com/jq/download/"
+   	exit
+fi
+
+OPENIDM_SERVER=$(jq '.server' settings.json | sed 's/\"//g')
+OPENIDM_SERVER_PORT=$(jq '.port' settings.json)
 
 #check that arg is passed
 if [ "$1" = "" ]; then
