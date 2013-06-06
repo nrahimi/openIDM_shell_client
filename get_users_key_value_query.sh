cc3339048ff5d025e7691e059c253854244830b3
smof <simon_moffatt@warpmail.net>
2013-06-06 12:04:36 +0100

    updated get_users_key_value_query to encode URL if it contains whitespace

--- get_users_key_value_query.sh
+++ get_users_key_value_query.sh
@@ -10,22 +10,27 @@ if [ "$JQ_LOC" = "" ]; then
    	exit
 fi
 
+#suck in username and password details
 OPENIDM_SERVER=$(jq '.server' settings.json | sed 's/\"//g')
 OPENIDM_SERVER_PORT=$(jq '.port' settings.json)
 
 #check that arg is passed
 if [ "$1" = "" ]; then
 	echo "Argument missing.  Requires field attribute as arg 1"
-	echo "Eg. ./get_users_key_value_query.sh familyName Smith"
+	echo "Eg. $0 familyName Smith"
 	exit
 fi
 
 #check that arg is passed
 if [ "$2" = "" ]; then
-	echo "Argument missing.  Requires field value as arg 2"
-	echo "Eg. ./get_users_key_value_query.sh familyName Smith"
+	echo "Argument missing.  Requires field value as arg 2.  Pass strings with spaces in quotes"
+	echo "Eg. $0 familyName Smith"
 	exit
 fi
 
-./geturl.sh "http://$OPENIDM_SERVER:$OPENIDM_SERVER_PORT/openidm/managed/user?_queryId=get-by-field-value&field=$1&value=$2"
+URL="http://$OPENIDM_SERVER:$OPENIDM_SERVER_PORT/openidm/managed/user?_queryId=get-by-field-value&field=$1&value=$2"
+#re-encode url not to contain spaces
+URL=$( printf "%s\n" "$URL" | sed 's/ /%20/g' )
+
+./geturl.sh $URL
 
