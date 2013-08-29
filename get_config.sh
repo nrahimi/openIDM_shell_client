81a1c376c50be4f806df7bd154f7cec8ada8cda0
smof <simon_moffatt@warpmail.net>
2013-08-29 18:46:00 +0100

    refactored get_config.sh

---
+++ get_config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#OpenIDM Shell Client
+#https://github.com/smof/openIDM_shell_client
+#Returns configuration object for given type
+
+#pulls in settings file
+source settings.sh
+
+#check that arg is passed	
+if [ "$1" = "" ]; then
+	echo ""
+	echo "Argument missing.  Requires name of conf object as arg 1"
+	echo "Eg. sync, managed, router, cluster, audit, authentication, scheduler, workflow"
+	echo "Eg. $0 sync"
+	echo ""
+	exit
+fi
+
+./geturl.sh "http://$OPENIDM_SERVER:$OPENIDM_SERVER_PORT/openidm/config/$1"
+
+
+
