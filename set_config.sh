767ca3d126d69067323fecf8c232709e36487013
smof <simon_moffatt@warpmail.net>
2013-09-04 16:34:19 +0100

    added set_config script

---
+++ set_config.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#OpenIDM Shell Client
+#https://github.com/smof/openIDM_shell_client
+#Sends given configuration object to OpenIDM
+
+#pulls in settings file
+source settings.sh
+
+#check that arg is passed	
+if [ "$1" = "" ]; then
+	echo ""
+	echo "Argument missing.  Requires name of conf object as arg 1"
+	echo "Eg. sync, managed, router, cluster, audit, authentication, scheduler, workflow"
+	echo "Eg. $0 sync.json"
+	echo ""
+	exit
+fi
+
+#strip out just basename, removing extension
+BASENAME=$(basename $1 .json)
+
+#url needs just basename
+URL="http://$OPENIDM_SERVER:$OPENIDM_SERVER_PORT/openidm/config/$BASENAME"
+
+#data needs appending with @ so that curl can pick it up
+DATA="@$1"
+
+#send to server
+./puturl.sh $URL $DATA
+
+
+
+
+
